7ff9b7f44f56e9c201e1b80cf7e2abc8cd7fde6b
Christian Soler <solero93@gmail.com>
2020-04-18 20:30:19 +0200

    Spring initializr created project

---
+++ src/main/kotlin/com/euvsvirus/euvsvirus/EuvsvirusApplication.kt
@@ -0,0 +1,11 @@
+package com.euvsvirus.euvsvirus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EuvsvirusApplication
+
+fun main(args: Array<String>) {
+	runApplication<EuvsvirusApplication>(*args)
+}
---
+++ src/test/kotlin/com/euvsvirus/euvsvirus/EuvsvirusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.euvsvirus.euvsvirus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EuvsvirusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
