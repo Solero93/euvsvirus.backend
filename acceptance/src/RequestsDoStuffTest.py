0e9a44dfb9aa16da9fc50cb6b3ccd328347d0b17
Christian Soler <solero93@gmail.com>
2020-04-24 22:53:34 +0200

    Added options test of login

--- acceptance/src/RequestsDoStuffTest.py
+++ acceptance/src/RequestsDoStuffTest.py
@@ -15,6 +15,9 @@ class RequestsDoStuff(unittest.TestCase):
     def test_option_request_to_create_user(self):
         self.assertEqual(requests.options(BASE_URL + "/api/user").status_code, 200)
 
+    def test_option_request_to_login_user(self):
+        self.assertEqual(requests.options(BASE_URL + "/api/user/login").status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
