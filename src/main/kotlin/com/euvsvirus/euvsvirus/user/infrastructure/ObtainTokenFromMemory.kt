0c19fd1a840fac99452617b36425982dd6da9a01
Christian Soler <solero93@gmail.com>
2020-04-25 22:08:03 +0200

    Throwing correct exception when cannot obtain user from email and password

--- src/main/kotlin/com/euvsvirus/euvsvirus/user/infrastructure/ObtainTokenFromMemory.kt
+++ src/main/kotlin/com/euvsvirus/euvsvirus/user/infrastructure/ObtainTokenFromMemory.kt
@@ -5,13 +5,15 @@ import com.euvsvirus.euvsvirus.user.domain.ObtainTokenRepository
 import com.euvsvirus.euvsvirus.user.infrastructure.inmemorydatabase.DatabaseUser
 import com.euvsvirus.euvsvirus.user.infrastructure.inmemorydatabase.TokenDatabase
 import com.euvsvirus.euvsvirus.user.infrastructure.inmemorydatabase.UserDatabase
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Repository
+import org.springframework.web.server.ResponseStatusException
 
 @Repository
 class ObtainTokenFromMemory: ObtainTokenRepository {
     override fun getToken(loginUserRequest: LoginUserRequest): String {
         val user: DatabaseUser = UserDatabase.getUserByEmailPassword(email = loginUserRequest.email, password = loginUserRequest.password)
-                ?: throw RuntimeException() // TODO Throw proper exception
+                ?: throw ResponseStatusException(HttpStatus.FORBIDDEN)
         return TokenDatabase.generateTokenForUser(user.id)
     }
 }
